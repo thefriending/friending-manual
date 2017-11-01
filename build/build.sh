175ea94ce639e0d145fe3ef6e21fec32e76e27bc
jrbeverly <jrbeverly@live.com>
2017-11-01 17:44:40 -0400

    Polishing the architecture

--- build/build.sh
+++ build/build.sh
@@ -9,10 +9,12 @@ DIR="$(dirname $SCRIPT)"
 ROOT_DIR="$(dirname $DIR)"
 SRC_DIR="${ROOT_DIR}/src"
 BIN_DIR="${ROOT_DIR}/output"
+JOBNAME="${BIN_DIR}/manual"
 
 # Build
 #
 # Build the latex project
 cd ${SRC_DIR}
-latexmk -pdf manual.tex
-mv manual.pdf ../manual.pdf
+mkdir -p "${BIN_DIR}"
+latexmk -pdf -outdir=out manual.tex
+mv out/manual.pdf ../manual.pdf
