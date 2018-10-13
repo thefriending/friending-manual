7f1b2e4e0ce672144c10b11153a4eb725e58e4ac
jrbeverly <jrbeverly@live.com>
2018-10-12 20:46:50 -0400

    Add in the screenshots

--- build/start.sh
+++ build/start.sh
@@ -6,16 +6,8 @@ set -e
 # Build variables for directories.
 SCRIPT=$(readlink -f "$0")
 DIR="$(dirname $SCRIPT)"
-IMAGE=jrbeverly/friending-ci
-
-# 
-# Environment
-#
-if [ -z $(docker images -q $IMAGE) ]; then
-   echo "The image $IMAGE does not exist locally, creating one now:"
-   sh $DIR/create.sh
-fi
+IMAGE=jrbeverly/latex:full
 
 docker run --rm -it \
         -v $(dirname $DIR):/media \
-        jrbeverly/friending-ci sh
+        $IMAGE sh
